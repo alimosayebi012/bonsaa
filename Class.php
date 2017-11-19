ad0f0775a585a300579479b43ca396febf6bca70
alimosayebi012 <alimosayebi012@gmail.com>
2017-11-19 10:31:18 +0330

    Update Class.php

--- Class.php
+++ Class.php
@@ -1,10 +1,10 @@
 <?php
 ob_start();
-define('API_KEY','توکن');
+define('460835958:AAFfHz1Pr-yLTxhUJzNoyL1CRur7echkjxw');
 /* تیم سازنده : @Lite_Team
    نویسنده : @mohammadstar_98 */
 $token = API_KEY;
-$admin =  "ایدی عددی ادمین";
+$admin =  356850459;
 $GetINFObot = json_decode(file_get_contents("https://api.telegram.org/bot".API_KEY."/getMe"));
 $UserNameBot = $GetINFObot->result->username;
 $NameBot = $GetINFObot->result->first_name;
